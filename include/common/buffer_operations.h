15b03334b8ebc7772e80698ad14689f2d0e4821f
Braedy Kuzma <braedy@ualberta.ca>
2017-08-13 00:03:06 -0600

    Add hash buffer operations.

--- include/common/buffer_operations.h
+++ include/common/buffer_operations.h
@@ -6,11 +6,17 @@
 
 namespace sc2tm {
 
-//! Write a uint32_t to a stream in network byte order
+//! Write a uint32_t to a stream in network byte order.
 void writeUint32(uint32_t val, std::ostream &os);
 
-//! Read a uin32_t from a stream in network byte order
+//! Read a uin32_t from a stream in network byte order.
 uint32_t readUint32(std::istream &is);
+
+//! Write a hash buffer to a stream.
+void writeHashBuffer(const uint8_t * const buffer, std::ostream &os);
+
+//! Read a hash buffer from a stream.
+void readHashBuffer(uint8_t *buffer, std::istream &is);
 
 } // End namespace sc2tm
 
--- src/common/buffer_operations.cpp
+++ src/common/buffer_operations.cpp
@@ -1,4 +1,5 @@
 #include "common/buffer_operations.h"
+#include "common/sha256.h"
 
 void sc2tm::writeUint32(uint32_t val, std::ostream &os) {
   val = htonl(val);
@@ -9,4 +10,12 @@ uint32_t sc2tm::readUint32(std::istream &is) {
   uint32_t value = 0;
   is.read((char *) &value, sizeof(value));
   return ntohl(value);
+}
+
+void sc2tm::writeHashBuffer(const uint8_t * const buffer, std::ostream &os) {
+  os.write((const char *) buffer, SHA256::DIGEST_SIZE);
+}
+
+void sc2tm::readHashBuffer(uint8_t *buffer, std::istream &is) {
+  is.read((char *) buffer, SHA256::DIGEST_SIZE);
 }
