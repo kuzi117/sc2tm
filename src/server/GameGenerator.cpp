38f240c5cf3c0c33487038f3aedbf887749f1062
Braedy Kuzma <braedy@ualberta.ca>
2017-08-20 23:55:25 -0600

    Add iterator include.

--- src/server/GameGenerator.cpp
+++ src/server/GameGenerator.cpp
@@ -4,6 +4,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <iterator>
 
 sc2tm::GameGenerator::GameGenerator(const SHAFileMap &botMap, const SHAFileMap &mapMap) {
   for (auto it : botMap)
